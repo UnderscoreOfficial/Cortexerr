87bc864d1072cdf4a7dcc4e2a5a33ed9b8fb10f8
UnderscoreOfficial <social.github_hh0n3q@underscoreofficial.com>
2026-03-23 17:48:48 -0400

    Support queued request via scheduler, minor feature finish
    RequestJob items now will be processed in a queue and by default will only dispatch out 2 concurrent running jobs at a time this allows requested jobs to naturally run until they fully complete and also helps limit bandwith saturation problems of having a large ammount of downloading items at once. Fixed various class and records that should have been sealed. Updated Indexer return data structure to better work with the ep based logic in series to better know when a state is finished. Other small changes.

--- Cortexerr.Extended/DataStructures.cs
+++ Cortexerr.Extended/DataStructures.cs
@@ -1,15 +1,17 @@
 using Cortexerr.Core.Ingest;
+using Cortexerr.Extended.Downloader;
 using Cortexerr.Extended.Indexer;
 
 namespace Cortexerr.Extended.DataStructures;
 
-public record RequestJob
+public sealed record RequestJob
 {
     public required Ingest ingest { get; init; }
     public List<IndexerSearchJob> indexer_search_jobs { get; init; } = new();
+    public List<DownloadJob> download_jobs { get; init; } = new();
 }
 
-public record DownloadJob
-{
-    public required Ingest ingest { get; init; }
-}
+// public class DownloaderJobs
+// {
+//     public required List<DownloadJob> download_job { get; init; } = new();
+// }
